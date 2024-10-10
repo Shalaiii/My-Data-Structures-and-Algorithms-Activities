ba204c5fbfaa370d91f2e05ab2443d470c471371
Shalaiii <gshalaikamae@gmail.com>
2024-10-10 23:36:45 +0800

    Add files via upload

---
+++ arrayInitialization.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class arrayInitialization{
+	public static void main(String [] args){
+		int[] num = {1, 2, 3, 4, 5};
+
+		for (int a = 0; a < num.length; a++){
+			System.out.println(num[a]);
+			}
+		}
+	}
