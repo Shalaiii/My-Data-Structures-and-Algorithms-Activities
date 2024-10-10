d9334430998562b763424058352a6c5bd13d1549
Shalaiii <gshalaikamae@gmail.com>
2024-10-10 23:37:26 +0800

    Add files via upload

---
+++ printArrayElements.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class printArrayElements{
+	public static void main(String[]args){
+
+		String[] pens = {"FlexStick", "Farber Castel", "Test good", "Panda", "HBW"};
+
+		for (int a = 0; a < pens.length; a++){
+			System.out.println("Element at index ["+a+"]: "+pens[a]);
+			}
+		}
+	}
