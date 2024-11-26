ead5289435110f933945351efec395fdc2458529
Shalaiii <gshalaikamae@gmail.com>
2024-11-26 10:16:24 +0800

    Update arrayInitialization.java

--- arrayInitialization.java
+++ arrayInitialization.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
 
-public class arrayInitialization{
-	public static void main(String [] args){
-		int[] num = {1, 2, 3, 4, 5};
+public class arrayInitialization {
+    public static void main(String[] args) {
+        // Declare and initialize an integer array with predefined values
+        int[] num = {1, 2, 3, 4, 5};
 
-		for (int a = 0; a < num.length; a++){
-			System.out.println(num[a]);
-			}
-		}
-	}
+        // Use a for loop to iterate through the array
+        for (int a = 0; a < num.length; a++) {
+            // Print the current element of the array to the console
+            System.out.println(num[a]);
+        }
+    }
+}
